c4fc0759622accae4040a588df4babc29e07656d
Szymon Wawrzyczek <36824343+szwawrzyczek@users.noreply.github.com>
2019-03-11 11:52:47 +0100

    Johnson algorithm edded

--- Lab1.cpp
+++ Lab1.cpp
@@ -1,6 +1,13 @@
-#include "stdafx.h"
+//#include "stdafx.h"
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <vector>
 
-//using namespace std;
+using namespace std;
+
+
 
 std::vector<std::vector<int>> matrix;
 std::vector<std::vector<int>> data2 = { { 4, 5 },{ 4, 1 },{ 10, 4 },{ 6, 10 },{ 2, 3 } };
@@ -174,17 +181,82 @@ void reduce_col(std::vector<std::vector<int>> arr)
 	}
 }
 
-void johnson_algorithm()
-{
-
+/*
+	@johnson_algorithm function
+	need parameters: int Table[n][2]			  - Table n task for 2 machins which represent time
+					  vector<int>& task_sequence  - References to result sequences of tasks
+					  int n						  - Amount of tasks
+*/
+void johnson_algorithm(int Tablica[][2], vector<int>& task_sequence,int n){
+	queue <int> first_tasks;		//tasks on 1 machine
+	stack <int> second_tasks;		//task on second machine
+//	vector <int> task_sequence;		//first+second tasks 
+	bool tasks_list[5];				// tasks to do
+	int prev_min=0;					//prev task min
+	int which_task,which_machine=0;
+	fill_n(tasks_list,n,1);	        //fill table with n elem. Set on 1		
+	for(int iter=0;iter<n;iter++) 
+	{
+		int min=1000;		//1000 some great value;
+		for(int i=0; i<n;i++)
+		{
+			if (tasks_list[i]==false)	//if done serach in next one
+			continue;
+			for(int j=0; j<2; j++)
+			{	
+				if((Tablica[i][j])<=min)
+				{
+				 	min = Tablica[i][j];  //new min
+				 	which_task=i+1;which_machine=j;
+					 /*	 		unnecessary code?!
+				 	if(prev_min==min)
+					{	
+						tasks_list[i]=false;
+					 	if(j==0)
+					 	{
+					 		first_tasks.push(i+1);
+		 		
+						 }
+					 	else 				 		
+							{
+	 						second_tasks.push(i+1);
+		 					}
+					}*/	
+				}
+			}		
+		}
+		tasks_list[which_task-1]=false;		//task done
+	 	if(which_machine==0)	 
+			first_tasks.push(which_task);
+	 	else
+	 		second_tasks.push(which_task);	
+	}	
+	for(int i=0; !first_tasks.empty();i++)
+	{
+		task_sequence.push_back(first_tasks.front());
+		first_tasks.pop(); 
+	}	
+	for(int i=0; !second_tasks.empty();i++)
+	{
+		task_sequence.push_back(second_tasks.top());
+		second_tasks.pop();		
+	}
+	cout<<"Optymalna Sekwencja wykonania zadan(Algorytm Johnsona) to"<<endl;
+ 	for( int i = 0;i<task_sequence.size(); i++ )
+       cout<<" "<< task_sequence[i];
+    cout<<endl;
 }
 
 int main()
 {
 	int arr[5] = { 0,1,2,3,4 };
-	//int data[5][2] = { { 4, 5 },{ 4, 1 },{ 10, 4 },{ 6, 10 },{ 2, 3 } };
+	int data[5][2] = { { 4, 5 },{ 4, 1 },{ 10, 4 },{ 6, 10 },{ 2, 3 } };
+	int data2[5][2]={{9,2},{5,7},{8,7},{4,5,},{3,1}};
 	int n = sizeof arr / sizeof arr[0];
 	std::vector<int> combination = { 4,3,2,1,0 };
+	vector <int> task_sequence;
+	
+	
 	//print_matrix();
 	//print_matrix_universal(combination_to_jobs(combination));
 	//int jobs = 5;
@@ -194,6 +266,7 @@ int main()
 	all_combination_c_time();
 	print_matrix();
 	find_min_cmax();
+	johnson_algorithm(data2,task_sequence,5);
 	//reduce_col(data3);
 	//std::cout << count_time(table) << '\n';
 	//system("pause");
